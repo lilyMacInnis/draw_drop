92373c016f645666704491702aa70fb417930f28
LilyMacInnis <lilymacinnis@gmail.com>
2025-06-07 13:29:09 -0400

    create login and logout controllers

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -51,10 +51,40 @@ export const signup = async (req, res) => {
     }
 };
 
-export const login = (req, res) => {
-    res.send("login route");
+export const login = async (req, res) => {
+    const {email, password} = req.body;
+    try{
+        const user = await User.findOne({email});
+
+        if(!user){
+            return res.status(400).json({message: "Invalid credentials"});
+        };
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password);
+        if(!isPasswordCorrect){
+            res.status(400).json({message: "Invalid credentials"});
+        }
+
+        generateToken(user._id, res);
+
+        res.status(200).json({
+            _id: user._id,
+            userName: user.userName,
+            email: user.email,
+        });
+    } catch (error){
+        console.log("Error in login controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 };
 
 export const logout = (req, res) => {
-    res.send("logout route");
+    try{
+        // clear cookie
+        res.cookie("jwt", "", {maxAge: 0});
+        res.status(200).json({message: "Logged out successfully"});
+    }catch (error) {
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
 };
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId, res) => {
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
         httpOnly: true,
         sameSite: "strict",
-        secure: process.env.NODE_ENV !== "developement"
+        secure: process.env.NODE_ENV === "developement"
     });
 
     return token;
