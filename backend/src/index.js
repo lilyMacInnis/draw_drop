2a1a6f84c9d775466d74336fcaf131c00fc62b99
LilyMacInnis <lilymacinnis@gmail.com>
2025-06-07 13:40:44 -0400

    use cors and ignore .env

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import {connectDB} from './lib/db.js'
 
 import authRoutes from './routes/auth.routes.js';
@@ -9,6 +10,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+// middleware
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
