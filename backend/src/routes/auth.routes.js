68490717fd87e4cee140c6d00a6297594c2edc77
LilyMacInnis <lilymacinnis@gmail.com>
2025-06-08 11:15:22 -0400

    import checkAuth

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signup, login, logout } from '../controllers/auth.controllers.js';
+import { signup, login, logout, checkAuth } from '../controllers/auth.controllers.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
 
 const router = express.Router();
