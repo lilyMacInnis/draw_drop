d1404a9a9f1ac90b569b9d393c02f6c305ec6913
LilyMacInnis <lilymacinnis@gmail.com>
2025-06-09 13:01:02 -0400

    add origin to cors and set cors only for development

--- backend/src/index.js
+++ backend/src/index.js
@@ -13,7 +13,11 @@ const app = express();
 const PORT = process.env.PORT || 5001;
 
 // middleware
-app.use(cors());
+if(process.env.NODE_ENV === "development"){
+    app.use(cors({
+        origin: "http://localhost:5173"
+    }));
+}
 app.use(express.json());
 app.use(cookieParser());
 
