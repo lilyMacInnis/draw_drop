586aa74e607a5426eb8810e15562698abff219d0
LilyMacInnis <lilymacinnis@gmail.com>
2025-06-09 12:42:09 -0400

    setup home, signin, and login pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+import LoginPage from './pages/LoginPage'
 
 const App = () => {
   return (
-    <div className='text-red-500'>
-      tatsuo
+    <div >
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+
+      </Routes>
+
     </div>
   )
 }
