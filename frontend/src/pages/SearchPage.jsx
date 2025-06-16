ce0dfc1e7fdc707b00ae67f163acd490a061e59e
LilyMacInnis <lilymacinnis@gmail.com>
2025-06-16 14:00:27 -0400

    add users to searchpage

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react'
 import { useDrawStore } from '../store/useDrawStore'
+import { useNavigate } from 'react-router';
 
 const SearchPage = () => {
-  const {users, isLoadingUsers, getUsers} = useDrawStore();
+  const {users, isLoadingUsers, getUsers, setSelectedUser} = useDrawStore();
+  const navigate = useNavigate();
 
   useEffect( () => {
     getUsers();
   }, [getUsers]);
 
+  const handleClick = (user) => {
+    setSelectedUser(user);
+    navigate(`/send/${user._id}`);
+
+  }
+
   if(isLoadingUsers){
-    return <div>Lodaing ...</div>
+    return <div>Loading ...</div>
   };
 
   return (
@@ -20,7 +28,15 @@ const SearchPage = () => {
       />
 
       <div>
-        
+        {users.map((user) => (
+            <button
+                key = {user._id}
+                onClick={() => handleClick(user)}
+            >
+                <div>{user.userName}</div>
+                <div>{user.email}</div>
+            </button>
+        ))}
       </div>
     </div>
   )
