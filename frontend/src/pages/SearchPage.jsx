6ce1369936f2ffd8abd4e125610e7f5c9e659a8c
LilyMacInnis <lilymacinnis@gmail.com>
2025-06-16 13:36:32 -0400

    setup search page

--- frontend/src/pages/SearchPage.jsx
+++ frontend/src/pages/SearchPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDrawStore } from '../store/useDrawStore'
 
 const SearchPage = () => {
+  const {users, isLoadingUsers, getUsers} = useDrawStore();
+
+  useEffect( () => {
+    getUsers();
+  }, [getUsers]);
+
+  if(isLoadingUsers){
+    return <div>Lodaing ...</div>
+  };
+
   return (
     <div>
-      search 
+      {/* TODO: implement search */}
+      <input 
+        placeholder='Search for a User'
+      />
+
+      <div>
+        
+      </div>
     </div>
   )
 }
