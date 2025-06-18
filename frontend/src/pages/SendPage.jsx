cf08d7a9258e4598a0175e21c57abedd9ab5d8bc
LilyMacInnis <lilymacinnis@gmail.com>
2025-06-18 13:17:35 -0400

    make sendPage

--- frontend/src/pages/SendPage.jsx
+++ frontend/src/pages/SendPage.jsx
@@ -1,10 +1,66 @@
 import React, { useState } from 'react'
+import { useDrawStore } from '../store/useDrawStore';
 
 const SendPage = () => {
   const [imageUrl, setImageUrl] = useState("");
+  const [isAnon, setisAnon] = useState(true);
+  const {sendDrawing, isSendingDrawing} = useDrawStore();
+
+  const handleSendMessage = async (e) => {
+    e.preventDefault();
+    if(!imageUrl) return;
+
+    try{
+      await sendDrawing({
+        image: imageUrl,
+        isAnon,
+      })
+    } catch (error){
+      console.log("Failed to send drawing: ", error);
+    }
+  };
+
   return (
     <div>
-      send
+      <form onSubmit={handleSendMessage}>
+        <button onClick={setisAnon(!isAnon)}>
+          Anon:
+          {
+            isAnon ? (
+              <>
+                yes
+              </>
+            ) : (
+              <>
+                no
+              </>
+            )
+          }
+        </button>
+        <input
+          type='text'
+          placeholder='imageurl'
+          value={imageUrl}
+          onChange={(e) => setImageUrl(e.target.value)}
+        />
+
+        <button
+          type='submit'
+          disabled={!imageUrl.trim() || isSendingDrawing}
+        >
+          {
+            isSendingDrawing ? (
+              <>
+                Sending...
+              </>
+            ) : (
+              <>
+                Send
+              </>
+            )
+          }
+        </button>
+      </form>
     </div>
   )
 }
