2fdc842e516830da000dff02284cf02a5ffb46a2
LilyMacInnis <lilymacinnis@gmail.com>
2025-06-20 13:17:06 -0400

    create get user from is function

--- backend/src/controllers/drawing.controllers.js
+++ backend/src/controllers/drawing.controllers.js
@@ -12,6 +12,20 @@ export const getAllUsers = async (req, res) => {
     }
 };
 
+export const getUserFromId = async (req, res) => {
+    try{
+        const {id} = req.body;
+        const user = await User.findById(id);
+
+        if(user){
+            res.status(200).json(user)
+        }
+    } catch (error){
+        console.log("Error in getUserFromId: ", error.message);
+        res.status(500).json({message: "Internal Server Error"});
+    }
+};
+
 export const getDrawingsSentToUser = async (req, res) => {
     try{
         const receiverId = req.user._id;
