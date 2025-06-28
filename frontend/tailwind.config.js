5e00cd8b2aa5813127322590b6a23f0e9e3e7197
LilyMacInnis <lilymacinnis@gmail.com>
2025-06-28 13:09:57 -0400

    add dark and light themes

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "rgba(var(--background))",
+        bgUltra: "rgba(var(--bg-ultra))",
+        primary: "rgba(var(--primary))",
+        primaryl: "rgba(var(--primary-light))",
+        primaryd: "rgba(var(--primary-dark))",
+        accent: "rgba(var(--accent))",
+      },
+    },
   },
   plugins: [],
 }
