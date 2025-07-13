6c5413f50559e4cc94c4c6f106193ec02a99fade
LilyMacInnis <lilymacinnis@gmail.com>
2025-07-13 17:01:02 -0400

    deployment preparations

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 import {connectDB} from './lib/db.js';
 
 import authRoutes from './routes/auth.routes.js';
@@ -11,9 +12,10 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-if(true){
+if(process.env.NODE_ENV !== "production"){
     app.use(cors({
         origin: "http://localhost:5173",
         credentials: true
@@ -35,9 +37,13 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/draw", drawingRoutes);
 
-app.get("/", (req, res) => {
-    res.send("test");
-});
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get(/(.*)/, (req, res) => {
+    res.sendFile(path.join(__dirname,"../frontend", "dist", "index.html"));
+  });
+};
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
